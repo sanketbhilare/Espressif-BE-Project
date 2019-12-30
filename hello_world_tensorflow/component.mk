fd7674b5eca3844f4ac5982b6b71da92fb5b30cf
jaysp98 <jaysp98@gmail.com>
2019-12-31 00:25:50 +0530

    Removed duplicates

--- hello_world_tensorflow/component.mk
+++
@@ -1,2 +0,0 @@
-COMPONENT_SRCDIRS:= . main/esp main tensorflow/lite/experimental/micro tensorflow/lite/experimental/micro/kernels tensorflow/lite/experimental/micro/memory_planner tensorflow/lite/c tensorflow/lite/core/api  tensorflow/lite/kernels/internal tensorflow/lite/kernels/
-COMPONENT_ADD_INCLUDEDIRS:= . main/esp main third_party/gemmlowp third_party/flatbuffers/include third_party/kissfft
--- hello_world_tensorflow/main/obj_model.h
+++
@@ -1,7 +0,0 @@
-#ifndef TENSORFLOW_LITE_EXPERIMENTAL_MICRO_EXAMPLES_OBJ_MODEL_DATA_H_
-#define TENSORFLOW_LITE_EXPERIMENTAL_MICRO_EXAMPLES_OBJ_MODEL_DATA_H_
-
-extern const unsigned char g_obj_model_data[];
-extern const int g_obj_data_len;
-
-#endif 
